e3729d0810eda54c56c666eb88aff5f681a2357e
Joseph Weidinger <josephweidinger@carfax.com>
2017-12-17 09:37:19 -0600

    made training batch be consistent but small to see progress and general code cleanup

--- cpp-piano-learning-cnn-data-provider/src/main.cpp
+++ cpp-piano-learning-cnn-data-provider/src/main.cpp
@@ -12,7 +12,7 @@ namespace py = pybind11;
 class PianoLearnerDataProvider {
 public:
     PianoLearnerDataProvider() {
-        float TRAINING_SIZE = 0.8;
+        // load things into memory
         allSamples = loadSamplesIntoMemory("/var/tmp/ivy/");
         map<string, int> sampleSizes = determineSampleSizes(allSamples);
         vectorOfVectorBufferEvents = loadMidiJsonIntoMemory("/Users/josephweidinger/Downloads/dum/", sampleSizes);
@@ -21,7 +21,6 @@ public:
         // get index arrays
         auto numTotalBuffers = (int) vectorOfVectorBufferEvents.size();
         randomIndexes = generateRandomIndexes(numTotalBuffers);
-
         auto numTrainingBuffers = (int) round(numTotalBuffers * TRAINING_SIZE);
 
         for (int i = 0; i < numTrainingBuffers; i++) {
@@ -30,12 +29,13 @@ public:
         for (int i = numTrainingBuffers; i < numTotalBuffers; i++) {
             testIndexes.push_back(randomIndexes[i]);
         }
-
+        miniTestIndexes = pickRandomIndexes(testIndexes, 500);
 
     }
     vector<vector<vector<float>>> getTrainingBatch(int batchSize);
-    vector<vector<vector<float>>> getRandomTestData(int batchSize);
+    vector<vector<vector<float>>> getMiniTestData();
     int BUFFER_SIZE = 1024;
+    float TRAINING_SIZE = 0.8;
 
 private:
     map<string, vector<float>> allSamples;
@@ -43,19 +43,13 @@ private:
     vector<int> randomIndexes;
     vector<int> trainingIndexes;
     vector<int> testIndexes;
+    vector<int> miniTestIndexes;
 };
 
 vector<vector<vector<float>>> PianoLearnerDataProvider::getTrainingBatch(int batchSize) {
     vector<vector<vector<float>>> allInputsAndLabelsForBatch;
 
     vector<int> thisBatchRandomIndexes = pickRandomIndexes(trainingIndexes, batchSize);
-
-    ////////////// TEMP PRINT START
-    // cout << "this training label batch size: " << batchSize << " of " << trainingIndexes.size() << endl;
-    // cout << "training indexes for this batch indexes: ";
-    // cout << endl;
-    // cout << "vectorOfVectorBufferEvents size: " << vectorOfVectorBufferEvents.size() << endl;
-    ////////////// TEMP PRINT END
 
     vector<vector<float>> inputs;
     vector<vector<float>> labels;
@@ -72,19 +66,12 @@ vector<vector<vector<float>>> PianoLearnerDataProvider::getTrainingBatch(int bat
    return allInputsAndLabelsForBatch;
 };
 
-vector<vector<vector<float>>> PianoLearnerDataProvider::getRandomTestData(int batchSize) {
+vector<vector<vector<float>>> PianoLearnerDataProvider::getMiniTestData() {
     vector<vector<vector<float>>> allInputsAndLabelsForTestData;
-    vector<int> thisBatchRandomIndexes = pickRandomIndexes(testIndexes, batchSize);
-    ////////////// TEMP PRINT START
-    // cout << "total test data: " << allInputsAndLabelsForTestData.size() << endl;
-    // cout << "test indexes: ";
-    // cout << endl;
-    ////////////// TEMP PRINT END
-
     vector<vector<float>> inputs;
     vector<vector<float>> labels;
 
-    for (auto & i : thisBatchRandomIndexes) {
+    for (auto & i : miniTestIndexes) {
         InputLabelPairing pair = processEvents(allSamples, vectorOfVectorBufferEvents[i], BUFFER_SIZE);
         inputs.push_back(pair.fftInput);
         labels.push_back(pair.ampLabel);
@@ -95,29 +82,6 @@ vector<vector<vector<float>>> PianoLearnerDataProvider::getRandomTestData(int ba
 
     return allInputsAndLabelsForTestData;
 };
-
-// vector<vector<BufferEvent>> PianoLearnerDataProvider::getVectorOfVectorBufferEvents() {
-//     return vectorOfVectorBufferEvents;
-// };
-
-
-
-// get random batch
-// 1. figure out length of all json BUFFERS (does it contain small buffer at end? hope not)
-// 2. make an array of indexes from 0 to that num
-// 3. shuffle that array on each cycle
-// 4. get the first 0:batch size numbers
-// that might be tricky
-// grab those 20 files, and go to town
-
-// training and test data
-// 1 idea: get that big array of numbers from above and take out 30% of them and put them in another array...
-// that will be the training set...
-
-// the problem with both of these, is we have to associte a big list of numbers as filename -> its list
-// but not just have a vector of Buffer events instead of separate files? combine them all?
-
-
 
 
 PYBIND11_MODULE(cpp_piano_learning_cnn_data_provider, m) {
@@ -137,7 +101,7 @@ PYBIND11_MODULE(cpp_piano_learning_cnn_data_provider, m) {
     py::class_<PianoLearnerDataProvider> pianoLearnerDataProvider(m, "PianoLearnerDataProvider");
     pianoLearnerDataProvider.def(py::init<>());
     pianoLearnerDataProvider.def("getTrainingBatch", &PianoLearnerDataProvider::getTrainingBatch);
-    pianoLearnerDataProvider.def("getRandomTestData", &PianoLearnerDataProvider::getRandomTestData);
+    pianoLearnerDataProvider.def("getMiniTestData", &PianoLearnerDataProvider::getMiniTestData);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
