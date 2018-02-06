2d86d54973b5b2cf1191e551e0d1cc17d9ff8436
Joseph Weidinger <josephweidinger@carfax.com>
2018-02-06 08:01:50 -0600

    naive approach

--- scripts/infer.py
+++ scripts/infer.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 from nn import nn
 import wave
 import math
-import scipy.io.wavfile
 
 WAV_FILE_PATH = sys.argv[1]
 MODEL_PATH = sys.argv[2]
@@ -51,6 +50,26 @@ for i in range(num_even_buffers):
 	raw_prediction = nnSess.runGraph(this_buffer_fft_reshaped.astype(float), 1.0)
 	predictions.append(raw_prediction[0])
 
+def make_wave_from_intensity_and_index(index, intensity):
+    frequency = 440 * (1.059463094359 ** (index - 49))
+    x = np.arange(BUFFER_LENGTH)
+    return np.sin(2 * np.pi * frequency * x / 44100) * intensity
+    
+master_signals = []
+
+for prediction in predictions:
+    signals = []
+    for index, intensity in enumerate(prediction):
+        signals.append(make_wave_from_intensity_and_index(index, intensity))
+    master_signals.append([sum(x) for x in zip(*signals)])
+
+final_signal = [item for sublist in master_signals for item in sublist]
+normalization_factor = np.amax(final_signal)
+final_signal = np.array(final_signal) * (1 / normalization_factor)
+
+import scipy.io.wavfile
+scipy.io.wavfile.write("./wavs/output.wav", 44100, final_signal)
+
 def plot_arbitrary_2d_data_as_spectrogram(data, x_label="X", y_label="Y"):
     
     x_size, y_size = np.shape(data)
@@ -77,3 +96,4 @@ def plot_arbitrary_2d_data_as_spectrogram(data, x_label="X", y_label="Y"):
     plt.clf()
 
 plot_arbitrary_2d_data_as_spectrogram(predictions, 'sample number', 'key')
+
